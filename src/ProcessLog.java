c4a3c1bcecbceef25ed886146406d351eaecc5d0
JustAnirudh <anirudh.pathak1@gmail.com>
2017-07-05 00:15:38 -0700

    populated alNeighbours

--- src/ProcessLog.java
+++ src/ProcessLog.java
@@ -1,9 +1,7 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by paanir on 7/4/17.
@@ -18,6 +16,32 @@ public class ProcessLog {
     int degree;
     int transactionsSize;
     int time;
+
+    public List<String> doBFSTillDegreeD(HashMap<String, GraphNode> graph, String source) {
+        GraphNode srcNode = graph.get(source);
+        srcNode.color = "gray"; //discovered
+        srcNode.distSrc = 0;
+        Queue<String> queue = new LinkedList<>();
+        List<String> res = new ArrayList<>();
+        queue.add(source);
+        while (!queue.isEmpty()) {
+            String curr = queue.remove();
+            if (graph.get(curr).distSrc == this.degree + 1) //if reached a node farther than given degree, break
+                break;
+            res.add(curr);
+            for (String neigh : graph.get(curr).neighbors) {
+                if (graph.get(neigh).color.equals("white")) { //undiscovered
+                    GraphNode neighNode = graph.get(neigh);
+                    neighNode.color = "gray";
+                    neighNode.distSrc = graph.get(curr).distSrc + 1;
+                    neighNode.parentString = curr;
+                    queue.add(neigh);
+                }
+            }
+            graph.get(curr).color = "black"; //finished
+        }
+        return res;
+    }
 
     public void setupInitialState(BufferedReader bufferedReader, ObjectMapper mapper) {
         try {
@@ -50,9 +74,9 @@ public class ProcessLog {
                         GraphNode node1 = new GraphNode(id1);
                         String id2 = befriend.id2;
                         GraphNode node2 = new GraphNode(id2);
-                        if(!idToNeighboursGraph.containsKey(id1))
+                        if (!idToNeighboursGraph.containsKey(id1))
                             idToNeighboursGraph.put(id1, node1);
-                        if(!idToNeighboursGraph.containsKey(id2))
+                        if (!idToNeighboursGraph.containsKey(id2))
                             idToNeighboursGraph.put(id2, node2);
                         node1.neighbors.add(id2);
                         node2.neighbors.add(id1);
@@ -73,6 +97,11 @@ public class ProcessLog {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        //now populate id to all neighbours map
+        for (String id : idToNeighboursGraph.keySet()) {
+            HashMap<String, GraphNode> idToNeighboursGraph = new HashMap<>(this.idToNeighboursGraph);
+            idToAllNeighbours.put(id, doBFSTillDegreeD(idToNeighboursGraph, id));
         }
 
     }
